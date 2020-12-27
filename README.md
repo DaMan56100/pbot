6d176f58f770899d5dc879faae9a80c638fb4fc2
DaMan56100 <samcraft792@gmail.com>
2020-12-27 19:12:27 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+it plays the popular 'penis music'
+
+what an awful bot
+
+uses .env variables GUILD_ID and CHANNEL_ID to specify which channel to play music to
---
+++ bot.ts
@@ -0,0 +1,49 @@
+import { Client, Guild, StreamDispatcher, VoiceChannel, VoiceConnection } from "discord.js";
+import { config } from "dotenv";
+config();
+
+const GUILD_ID = process.env.GUILD_ID ? process.env.GUILD_ID : "";
+const CHANNEL_ID = process.env.CHANNEL_ID ? process.env.CHANNEL_ID : "";
+
+const client = new Client();
+
+let stopper:StreamDispatcher | undefined = undefined;
+let connection:VoiceConnection | null = null;
+let penis:boolean = false;
+
+client.login();
+
+client.on('ready', async () => {
+    const {guild,channel} = await getDetails();
+
+    await (channel as VoiceChannel).join();
+});
+
+client.on('voiceStateUpdate',async (oldState, newState) => {
+    const {guild,channel} = await getDetails();
+
+    const members = channel.members.size;
+
+    if (members == 1) stopPenis();
+
+    if (newState.connection) connection = newState.connection;
+    if (members >= 2 && !penis) startPenis();
+})
+
+async function getDetails(): Promise<{guild:Guild,channel:VoiceChannel}> {
+    const guild = await client.guilds.fetch(GUILD_ID);
+    const channel = guild.channels.resolve(CHANNEL_ID);
+    if (!channel) throw ("Couldn't find channel");
+
+    return {guild: guild,channel: (channel as VoiceChannel)}
+}
+
+function startPenis() {
+    penis = true;
+    stopper = connection?.play("./penis.mp3").on("finish", () => startPenis());
+}
+
+function stopPenis() {
+    penis = false;
+    stopper?.pause();
+}
